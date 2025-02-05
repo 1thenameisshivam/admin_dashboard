8aba5381487823089e6005d194f221ca5ff0ad1e
shivam kumar <1thenameisshivam@gmail.com>
2025-02-05 23:11:59 +0530

    interseptor added for refresh token

--- src/http/client.js
+++ src/http/client.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
   withCredentials: true,
@@ -7,3 +8,25 @@ export const api = axios.create({
     Accept: "application/json",
   },
 });
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      try {
+        await axios.post(
+          `${import.meta.env.VITE_BACKEND_API_URL}/auth/refresh`,
+          {},
+          { withCredentials: true }
+        );
+        return api(originalRequest);
+      } catch (err) {
+        useAuthStore.getState().logout();
+        return Promise.reject(err);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
