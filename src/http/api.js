084b7608f2891e63783ea9a3babc53e87df46588
shivam kumar <1thenameisshivam@gmail.com>
2025-02-06 15:50:35 +0530

    add users tablw

--- src/http/api.js
+++ src/http/api.js
@@ -3,3 +3,4 @@ import { api } from "./client";
 export const login = (credential) => api.post("/auth/login", credential);
 export const self = () => api.get("/auth/self");
 export const logOut = () => api.post("/auth/logout");
+export const getUsers = () => api.get("/user");
--- src/pages/user/UsersPage.jsx
+++ src/pages/user/UsersPage.jsx
@@ -1,21 +1,64 @@
 import { RightOutlined } from "@ant-design/icons";
-import { Breadcrumb } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { Breadcrumb, Space, Table } from "antd";
 import { Link } from "react-router";
+import { getUsers } from "../../http/api";
 
 export const UsersPage = () => {
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      return getUsers().then((res) => res.data.users);
+    },
+  });
   return (
-    <div>
-      <Breadcrumb
-        separator={<RightOutlined />}
-        items={[
-          {
-            title: <Link to={"/"}>Home</Link>,
-          },
-          {
-            title: "Users",
-          },
-        ]}
-      />
-    </div>
+    <>
+      <Space direction="vertical" size={16} className="w-full">
+        <Breadcrumb
+          separator={<RightOutlined />}
+          items={[
+            {
+              title: <Link to={"/"}>Home</Link>,
+            },
+            {
+              title: "Users",
+            },
+          ]}
+        />
+        <div>
+          <Table dataSource={data} columns={columns} />;
+        </div>
+      </Space>
+    </>
   );
 };
+
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text, record) => (
+      <h1>
+        <Link to={`/users/${record.id}`}>
+          {record.firstName} {record.lastName}
+        </Link>
+      </h1>
+    ),
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
